8da5c7113fddd4a5e85c8979a8bbeeb5d140fb11
cyanglee <cyoung.lee@gmail.com>
2013-09-11 10:03:38 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bigheart::Application.config.secret_key_base = 'cdaebca1c3e6c8ba1c780cae8e7862314bcf2bcf90ff67ed61cb0b3696235aa1643ea0e11b0f730b18f8b05d85d5693aa868e7c58de5d8067a9d116485877777'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bigheart::Application.config.session_store :cookie_store, key: '_bigheart_session'
