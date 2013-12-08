0ecd2637439f749034d1a399a48aea329470bd37
marcus <marcus@visionbundles.com>
2013-12-09 00:29:23 +0800

    finish facbook log in back end part

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Bigheart::Application.routes.draw do
-  devise_for :users, :controllers => {:registrations => "registrations"}
+  devise_for :users, :controllers => {:registrations => "registrations", :omniauth_callbacks => "users/omniauth_callbacks"}
   resources :users
   resources :stories
 
