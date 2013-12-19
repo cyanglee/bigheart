17459320389edddfbc377e47ffd2bf9b5603bcbf
marcus <marcus@visionbundles.com>
2013-12-20 01:56:31 +0800

    fix state migration

--- db/migrate/20131219174738_change_state_to_int.rb
+++ db/migrate/20131219174738_change_state_to_int.rb
@@ -1,9 +1,9 @@
 class ChangeStateToInt < ActiveRecord::Migration
   def change
     connection.execute(%q{
-    alter table tweets
-    alter column number
-    type integer using cast(number as integer)
+    alter table stories
+    alter column state
+    type integer using cast(state as integer)
   })
   end
 end
