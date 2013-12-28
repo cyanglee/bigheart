9498ea9be83f861bfe19c0d302f28d884e8b91bd
marcus <marcus@visionbundles.com>
2013-12-28 17:32:02 +0800

    add domain to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Bigheart::Application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    #domain: ENV["DOMAIN_NAME"],
+    domain: 'http://bigheart.herokuapp.com/',
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV['MAIL_AD'],
