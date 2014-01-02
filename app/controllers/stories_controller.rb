1a296c517b8298cf2613f9b21ed1660a54c7c724
marcus <marcus@visionbundles.com>
2014-01-02 20:20:38 +0800

    add og tags to show stories, change production assets to true

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -9,6 +9,7 @@ class StoriesController < ApplicationController
 
   def show
     story = Story.find(params[:id])
+    set_meta_tags og: {title: "#{story.story_name}", description: "#{story.story_details}", type: "website", url: "http://bigheart.tw/stories/#{story.id}", image: {url: "http://bigheart.tw#{story.image}"}}
     if story.state != 2
       if user_signed_in? && current_user.has_role?(:admin)
         @story = story
