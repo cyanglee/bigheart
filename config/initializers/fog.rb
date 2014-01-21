2c9b8eaf97c77abfad616fb14baea18dd23bdfd4
marcus <marcus@visionbundles.com>
2014-01-21 23:10:11 +0800

    fix fog settings

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,10 +1,10 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    :aws_access_key_id => Settings.AWS_KEY,
-    :aws_secret_access_key => Settings.AWS_SECRET_KEY,
+    :aws_access_key_id => ENV['AWS_KEY'],
+    :aws_secret_access_key => ENV['AWS_SECRET_KEY'],
     :provider => 'AWS',
     :region => 'ap-northeast-1'
   }
-  config.fog_directory = Settings.BUCKET_NAME
+  config.fog_directory = ENV['BUCKET_NAME']
   config.fog_public = true
 end
